2253fbda885e93f200290c6b500170911c4dbfc7
Silas Bartol <92451943+silasiscool@users.noreply.github.com>
2023-01-30 10:31:52 -0500

    Add files via upload

---
+++ master.js
@@ -0,0 +1,42 @@
+let goButton = document.getElementById('go-button');
+let termInput = document.getElementById('term-input');
+let removeBlankTerms = document.getElementById('remove-blank-terms');
+let removeLastWord = document.getElementById('remove-last-word');
+let removeFirstWord = document.getElementById('remove-first-word');
+
+let openedTabs = [];
+
+goButton.addEventListener('click',()=>{
+  checkPopupPermissions();
+});
+
+function checkPopupPermissions(callback) {
+  window.open().close();
+  try {
+    window.open().close();
+    startTermSearch();
+  } catch {
+    setTimeout(function () {
+      alert('Popup permissions denied, allow then try again');
+    }, 10);
+  };
+};
+
+function startTermSearch() {
+  let terms = termInput.value.split('\n');
+  terms.forEach(term => {
+    if (!(removeBlankTerms.checked && term.length === 0)) {
+      formattedTerm = term;
+      if (removeLastWord.checked) {
+        formattedTerm = formattedTerm.substring(0, formattedTerm.lastIndexOf(" "));
+      };
+      if (removeFirstWord.checked) {
+        formattedTerm = formattedTerm.substring(formattedTerm.indexOf(" ")+1);
+      };
+      formattedTerm = formattedTerm.replace(/ /g, "+");
+      console.log(formattedTerm);
+      let currentWindow = window.open('https://www.google.com/search?q='+formattedTerm,'_blank');
+      openedTabs.push(currentWindow);
+    };
+  });
+}
